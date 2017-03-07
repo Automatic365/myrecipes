94ffe91af9b67caf1b1c250852ba19569f5df69e
Jason Hanna <jason.hanna01@gmail.com>
2017-03-07 13:11:57 -0700

    updated routes to resources - build out create method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,22 @@ class RecipesController<ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe Sucessfully Created"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:error] = ""
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 
 end
